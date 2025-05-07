e33c009c710266f561874c5daa7164e5bb1a72bf
Martina Kurkova <martinanoha@seznam.cz>
2025-05-07 16:02:07 +0200

    workshop_01

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,20 +1,17 @@
 import { render } from '@czechitas/render';
-import { WorkshopIntro } from '../components/WorkshopIntro';
-import { Venue } from "../components/Venue";
-import { Instructor } from '../components/Instructor';
 import '../global.css';
 import './index.css';
 
-const response = await fetch('http://localhost:4000/api/workshops/0');
-const json = await response.json();
-
-const data = json.data;
+const response = await fetch ("http://localhost:4000/api/workshops/0");
+const data = await response.json();
+console.log(data);
 
 document.querySelector('#root').innerHTML = render(
   <div className="container">
-    <WorkshopIntro title={data.title} description={data.description} />
-    <Venue name={data.venue.name} city={data.venue.city} street={data.venue.street} />
-    <Instructor name={data.instructor.name} bio={data.instructor.bio} />
+    <p className="text">{data.data.title}</p> 
+    <p>{data.data.description}</p>
+    <p>{data.data.instructor.name}</p>
+    <p>{data.data.venue.city}</p>
   </div>
 );
 
